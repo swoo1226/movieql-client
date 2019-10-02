1012ce3a788f784168faf760268f21125b2783de
swoo1226 <copycat1226@gmail.com>
2019-10-02 23:21:26 +0900

    how to insert variables into Query component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, {Component} from 'react';
-import client from './apolloClient';
-import  { ApolloProvider } from 'react-apollo';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
-import Detail from './Detail';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Router>
-          <Route exact={true} path={"/"} component={Home} />
-          <Route path={"/details/:movieId"} component={Detail} />
-        </Router>
+        <ApolloHooksProvider client={client}>
+          <Router>
+            <main>
+              <Route exact={true} path={"/"} component={Home} />
+              <Route path={"/details/:movieId"} component={Detail} />
+            </main>
+          </Router>
+        </ApolloHooksProvider>
       </ApolloProvider>
-      ) 
+    );
   }
 }
 
--- src/queries.js
+++ src/queries.js
@@ -2,11 +2,30 @@ import gql from 'graphql-tag';
 
 export const HOME_PAGE = gql`
     query {
-        movies(limit:50, rating:7) {
+        movies(limit:40, rating:8) {
             id
             title
-            genres
             rating
+            medium_cover_image
         }
     }
 `;
+
+export const MOVIE_DETAILS = gql`
+    query getMovieDetails($movieId: Int!) {
+        movie(id: $movieId) {
+            medium_cover_image
+            title
+            rating
+            description_intro
+            language
+            genres
+        }
+        suggestions(id: $movieId) {
+            id
+            title
+            rating
+            medium_cover_image
+        }
+    }
+`
